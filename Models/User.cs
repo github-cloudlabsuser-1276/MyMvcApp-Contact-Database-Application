99dcdfe4a66a43ed7a91631bcdbcc0803d88aadc
github_cloudlabsuser_1276 <github_cloudlabsuser_1276@cloudlabsaiuser.com>
2024-12-16 07:18:27 +0000

    Code for MVC Project with ARM Template

--- Models/User.cs
+++ Models/User.cs
@@ -1,6 +1,7 @@
 
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
@@ -9,7 +10,9 @@ namespace MyMvcApp.Models
     public class User
     {
         public int Id { get; set; }
+        [Required]
         public string Name { get; set; }
+        [Required]        
         public string Email { get; set; }
 
     }
