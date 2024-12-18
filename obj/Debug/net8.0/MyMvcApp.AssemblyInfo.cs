4fbda8f63fbbc23a88ed71c8ea244128a561a353
github-cloudlabsuser-1276 <168015235+github-cloudlabsuser-1276@users.noreply.github.com>
2024-12-18 09:23:28 +0000

    Update cache and configuration files for environment consistency

--- obj/Debug/net8.0/MyMvcApp.AssemblyInfo.cs
+++ obj/Debug/net8.0/MyMvcApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MyMvcApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+99dcdfe4a66a43ed7a91631bcdbcc0803d88aadc")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+441a7b918144ef5f79d24ba8fbd0a50f1d372a7c")]
 [assembly: System.Reflection.AssemblyProductAttribute("MyMvcApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MyMvcApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
