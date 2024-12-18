5eedaa717c6c431de821df0b30317bdec36306af
github-cloudlabsuser-1276 <168015235+github-cloudlabsuser-1276@users.noreply.github.com>
2024-12-18 09:56:23 +0000

    Remove old Azure deployment templates and add new parameters file for web app configuration

--- obj/Debug/net8.0/MyMvcApp.AssemblyInfo.cs
+++ obj/Debug/net8.0/MyMvcApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MyMvcApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4fbda8f63fbbc23a88ed71c8ea244128a561a353")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b6329de671bc4f09d49aa5211c6078114e4280ec")]
 [assembly: System.Reflection.AssemblyProductAttribute("MyMvcApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MyMvcApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
